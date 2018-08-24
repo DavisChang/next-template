d72d011d9ee25f6b19f3eb8a105a60cb8668521b
davis_chang <davis chang>
2018-08-24 21:15:21 +0800

    add intl

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,26 @@
 import React, { Component } from 'react';
+import { FormattedMessage, FormattedNumber, defineMessages } from 'react-intl';
+import pageWithIntl from '../components/PageWithIntl';
 import Layout from '../components/Layout';
 import { Link } from '../routes';
+
+const { description } = defineMessages({
+  description: {
+    id: 'description',
+    defaultMessage: 'An example app integrating React Intl with Next.js'
+  }
+})
 
 class Index extends Component {
 
   render() {
+    const { intl } = this.props;
     return (
       <Layout>
         <div>
           <h3>Index</h3>
-
+          <p>{intl.formatMessage(description)}</p>
+          <p><FormattedMessage id='greeting' defaultMessage='Hello, World!' /></p>
           <Link route="/code-splitting">
             <a>
               here
@@ -21,4 +32,4 @@ class Index extends Component {
   }
 }
 
-export default Index;
+export default pageWithIntl(Index);
